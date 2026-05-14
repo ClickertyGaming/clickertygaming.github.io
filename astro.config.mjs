edd231b3a720c4df7e38e1b2773de088aaa92285
ClickertyGaming <lovrejankovic1@gmail.com>
2026-05-14 10:54:40 +0200

    add db + update astro + move icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig, fontProviders } from "astro/config";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    site: "https://clickertygaming.github.io",
-    devToolbar: {
-        enabled: false
-    },
-    fonts: [{
-        provider: fontProviders.fontsource(),
-        name: "Roboto",
-        cssVariable: "--font-roboto"
-    }]
+  output: 'static',
+  site: "https://clickertygaming.github.io",
+
+  devToolbar: {
+      enabled: false
+  },
+
+  fonts: [{
+      provider: fontProviders.fontsource(),
+      name: "Roboto",
+      cssVariable: "--font-roboto"
+  }],
+
+  integrations: [db()]
 });
