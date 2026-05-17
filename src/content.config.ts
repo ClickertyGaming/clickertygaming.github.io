c9acf6256952fe62b81ad06924a7292d9bbb6fbd
ClickertyGaming <lovrejankovic1@gmail.com>
2026-05-17 12:28:34 +0200

    remove db, add reddit link, add collapsible header links, implement music archive finally

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 const blog = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog"}),
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
     schema: z.object({
         title: z.string(),
         description: z.string().default(""),
@@ -12,4 +12,18 @@ const blog = defineCollection({
     }),
     
 });
-export const collections = { blog };
+const music = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/music" }),
+    schema: z.object({
+        title: z.string(),
+        creator: z.string(),
+        year: z.number(),
+        genre: z.string(),
+        length: z.string(),
+        type: z.string(),
+        cover: z.string().default(""),
+        id: z.number(),
+        url: z.string()
+    })
+});
+export const collections = { blog, music };
